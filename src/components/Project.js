a750265bf68509f786a4defa3a8bcd3fc69dc5f6
SophiaGrace16 <sophia.g.white@outlook.com>
2021-01-12 15:58:37 -0500

    working on project pages

---
+++ src/components/Project.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Project = ({ project }) => {
+    return (
+    <li className="collection-item">
+      <b>{project.projectName}</b> <br />
+      <img src={project.imageLink}/><br />
+      Description: { project.summary}<br />
+      <a href={project.githubLink} target="_blank">Github Repository</a><br />
+      <a href={project.demoLink} target="_blank">Video Demonstration</a>
+    </li>
+    );
+}
+
+
+export default (Project)
--- src/reducers/educationReducer.js
+++ src/reducers/educationReducer.js
@@ -4,5 +4,6 @@ export default (state = [], action) => {
             return [...action.payload]
         default:
             return state
+            
     }
 }
