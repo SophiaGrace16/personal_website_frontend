2349fbea6a84b8b4bf206b071e5ecc34f1e57e4e
SophiaGrace16 <sophia.g.white@outlook.com>
2021-01-13 15:33:48 -0500

    adding individual project pages

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './containers/Home';
-import Link from './containers/Link';
 import Project from './containers/Project';
 import Resume from './containers/Resume';
+import ProjectShow from './containers/ProjectShow';
 import './App.css';
 
 class App extends Component {
@@ -22,10 +22,9 @@ class App extends Component {
           <div className="container">
             <Switch>
               <Route exact path="/" component={ Home } />
-              <Route exact path="/links" component={ Link } />
               <Route exact path="/resume" component={ Resume } />
               <Route exact path="/projects" component={ Project } />
-              {/* <Route exact path="/pets/:id" render={ props => <PetShow {...props} pets={ this.state.pets } />} /> */}
+              <Route exact path="/projects/:id" render={ props => <ProjectShow {...props} projects={ this.state.projects } />} />
             </Switch>
           </div>
           <Footer />
