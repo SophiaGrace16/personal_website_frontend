772acd38c789296f68c7973c39395e978e4a3ab2
SophiaGrace16 <sophia.g.white@outlook.com>
2021-01-15 14:04:06 -0500

    removing comments

--- src/containers/ProjectShow.js
+++ src/containers/ProjectShow.js
@@ -6,14 +6,6 @@ const ProjectShow = ({ match }) => {
   const {projectId}   = match.params
   const singleProj = parseInt(projectId)-1
   const project = useSelector(state => state.projects[singleProj])
-//   const comments = project.comments.map((comment) =>
-//     <div key={comment.id}>
-//         {comment.name}
-//         {comment.content}
-//     </div>
-//     );
-
-    //state.save -- need to save state so that it keeps performance normalized (normalizing data)
 
   if (!project) {
     return (
@@ -39,16 +31,10 @@ const ProjectShow = ({ match }) => {
 
             <a href={project.githubLink} target="_blank" ><img src="https://i.imgur.com/2Tqz4Yn.png" className="repo-button"/></a><br />
 
-            
             <div>
               <h5>Video Demonstration</h5>
-              <iframe width="560" height="315" src={project.demoLink} frameBorder="0" allow="accelerometer; autoplay; gyroscope; picture-in-picture" allowFullScreen />
+              <iframe width="560" height="315" src={project.demoLink} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
             </div>
-           
-
-            {/* <div className="project-spot-four">
-            {comments}
-            </div> */}
 
         </div>
        
@@ -56,7 +42,5 @@ const ProjectShow = ({ match }) => {
 
   )
 }
-
-
 
 export default ProjectShow
